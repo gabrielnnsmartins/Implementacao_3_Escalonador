74928a19df3aa747b16e8179fcd6f1cfedf295c2
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2026-09-08 21:24:07 -0300

    Um sisteminha que abre o arquivo e uma struct para as tarefas a seres executadas

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include<stdio.h>
+#include<string.h>
+
+typedef struct{
+    char nome[100];
+    int deadline, burst, periodo;
+}task;
 
 int main(int argc, char *argv[]){
-
+    if(argv != NULL && argc > 1){
+        if(strcmp(argv[1],"rate") == 0){
+            FILE *arquivo = fopen(argv[2],"r");
+            if(arquivo!=NULL){
+            int tempo_total = 
+            }
+        }
+    }
     return 0;
 }
